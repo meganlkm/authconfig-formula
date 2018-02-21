00d1bd58b863c552fe180b38f4e96323012925a0
Megan <megan.lkm@gmail.com>
2018-02-20 20:17:42 -0600

    add another test suite

---
+++ authconfig/tests/pytests/apply-single-sls-tests/test_000_apply_discovery.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from subprocess import check_output
+
+
+class ApplyDiscoveryTest(TestCase):
+
+    def test_apply_discovery(self):
+        state_apply_response = check_output(["salt-call", "--local", "state.apply", "authconfig.discovery"]).split('\n')
+        summary = state_apply_response[-8:]
+        failed = 0
+        for line in summary:
+            if line.startswith('Failed:'):
+                failed = int(line.split(':').pop().strip())
+        self.assertEqual(failed, 0)
