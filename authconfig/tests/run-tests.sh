87a15220c6a412799753fff3bf811e95f94f508b
Megan <megan.lkm@gmail.com>
2018-02-20 20:26:57 -0600

    only run discovery.sls test

--- authconfig/tests/run-tests.sh
+++ authconfig/tests/run-tests.sh
@@ -7,13 +7,13 @@ export OS_ID=$1
 
 cd ..
 
-# salt state.apply all tests
-docker run --rm \
-  -v $(pwd)/tests/pytests/apply-all-tests:/opt/tests \
-  -h "salt-state-testing-${OS_ID}" \
-  --name "salt-state-testing-${OS_ID}" \
-  -it authconfig:"salt-state-testing-${OS_ID}" \
-  pytest -s /opt/tests
+# # salt state.apply all tests
+# docker run --rm \
+#   -v $(pwd)/tests/pytests/apply-all-tests:/opt/tests \
+#   -h "salt-state-testing-${OS_ID}" \
+#   --name "salt-state-testing-${OS_ID}" \
+#   -it authconfig:"salt-state-testing-${OS_ID}" \
+#   pytest -s /opt/tests
 
 # salt state.apply single sls
 docker run --rm \
